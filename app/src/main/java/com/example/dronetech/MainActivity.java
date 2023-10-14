374406efe03b298ce56f932329554dbe4c28a057
Antoniobox <antonioboxsanchez@gmail.com>
2023-10-14 12:02:44 +0200

    Prueba de inicio de sesion completada

--- app/src/main/java/com/example/dronetech/MainActivity.java
+++ app/src/main/java/com/example/dronetech/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.dronetech;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.BaseTransientBottomBar;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText usuario;
+    private EditText password;
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        usuario = (EditText) findViewById(R.id.usuario);
+        password = (EditText) findViewById(R.id.password);
+        button = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = usuario.getText().toString();
+                String pass = password.getText().toString();
+                if(user.equals("hola") && pass.equals("adios")){
+                    Toast.makeText(getApplicationContext(), user, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
