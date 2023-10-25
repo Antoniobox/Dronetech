319cff70cc793125f0ae539dbd6e387fa52b3752
Antoniobox <antonioboxsanchez@gmail.com>
2023-10-25 10:16:54 +0200

    Eventos onClick y cambios de vista agregados al listado de drones

--- app/src/main/java/com/example/dronetech/ListadoDrones.java
+++ app/src/main/java/com/example/dronetech/ListadoDrones.java
@@ -2,13 +2,53 @@ package com.example.dronetech;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.JavascriptInterface;
+import android.widget.ImageView;
 
 public class ListadoDrones extends AppCompatActivity {
-
+    ImageView octocoptero;
+    ImageView tricoptero;
+    ImageView cuadricoptero;
+    ImageView hexacoptero;
+    private void changeView(Class clase){
+        Intent intent = new Intent(getApplicationContext(), clase);
+        startActivity(intent);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listado_drones);
+        octocoptero = findViewById(R.id.imgOctocoptero);
+        tricoptero = findViewById(R.id.imgTricoptero);
+        cuadricoptero = findViewById(R.id.imgCuadricoptero);
+        hexacoptero = findViewById(R.id.imgHexacoptero);
+        //TODO: cambiar las vistas cuando las tenga personalizadas
+        octocoptero.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeView(MainActivity.class);
+            }
+        });
+        tricoptero.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeView(MainActivity.class);
+            }
+        });
+        cuadricoptero.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeView(MainActivity.class);
+            }
+        });
+        hexacoptero.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeView(MainActivity.class);
+            }
+        });
     }
 }
